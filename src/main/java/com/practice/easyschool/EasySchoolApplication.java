9b0193faf0fb701f4de85f06b877b77639152b56
Panks <pankajsingla012@gmail.com>
2022-12-26 14:03:56 +0530

    Initial Commit : Creating SpringBoot Web Project using Spring Initializer

---
+++ src/main/java/com/practice/easyschool/EasySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.practice.easyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/easyschool/EasySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.easyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

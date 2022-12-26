e195127aa29e54230a8c14e3cd2e7dd4a6d51684
Panks <pankajsingla012@gmail.com>
2022-12-26 14:26:15 +0530

    Adding HomeController and home.html

---
+++ src/main/java/com/practice/easyschool/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.practice.easyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage() {
+        return "home.html";
+    }
+}

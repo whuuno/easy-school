7dd84825837d5921acd12b0cf61e6f9dea701e03
Panks <pankajsingla012@gmail.com>
2022-12-26 15:51:02 +0530

    Adding Thymeleaf Namespace and Introducing Model Class

--- src/main/java/com/practice/easyschool/controller/HomeController.java
+++ src/main/java/com/practice/easyschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.practice.easyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Pankaj");
         return "home.html";
     }
 }

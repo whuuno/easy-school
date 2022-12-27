f8554481df15a6eb75d871adc35262a3e9c9d303
Panks <pankajsingla012@gmail.com>
2022-12-27 12:20:23 +0530

    Adding WebMvcConfigurer for courses and about page

--- src/main/java/com/practice/easyschool/controller/HomeController.java
+++ src/main/java/com/practice/easyschool/controller/HomeController.java
@@ -4,20 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ViewController {
+public class HomeController {
 
     @RequestMapping(value = {"","/","/home"})
     public String displayHomePage() {
         return "home.html";
-    }
-
-    @RequestMapping(value = {"/courses"})
-    public String displayCoursePage() {
-        return "courses.html";
-    }
-
-    @RequestMapping(value = {"/contact"})
-    public String displayContactPage() {
-        return "contact.html";
     }
 }

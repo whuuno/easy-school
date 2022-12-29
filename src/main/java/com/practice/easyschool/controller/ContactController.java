ee232de885316ad66caea29ef48bf9554d1da9c9
Panks <pankajsingla012@gmail.com>
2022-12-29 11:47:07 +0530

    Adding Server Side Validation

--- src/main/java/com/practice/easyschool/controller/ContactController.java
+++ src/main/java/com/practice/easyschool/controller/ContactController.java
@@ -2,13 +2,18 @@ package com.practice.easyschool.controller;
 
 import com.practice.easyschool.model.Contact;
 import com.practice.easyschool.services.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class ContactController {
 
     private final ContactService contactService;
@@ -19,13 +24,18 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @PostMapping(value = "/saveMsg")
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute(name = "contact") Contact contact, Errors errors) {
+        if (errors.hasErrors()) {
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         boolean status = contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }

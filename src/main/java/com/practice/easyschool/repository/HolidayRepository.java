91bb1e8046e855896e2c6bf2af855d2c57610c3e
Panks <pankajsingla012@gmail.com>
2023-01-23 18:05:10 +0530

    Imports removal from HolidayRepository

--- src/main/java/com/practice/easyschool/repository/HolidayRepository.java
+++ src/main/java/com/practice/easyschool/repository/HolidayRepository.java
@@ -1,12 +1,10 @@
 package com.practice.easyschool.repository;
 
 import com.practice.easyschool.model.Holiday;
-import com.practice.easyschool.rowMapper.HolidayRowMapper;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.tree.RowMapper;
 import java.util.List;
 
 @Repository

3c4ec7e59328a9ae64a9468d7b2ea5f3cfae151a
Panks <pankajsingla012@gmail.com>
2023-01-24 22:42:28 +0530

    Adding Spring Data Jpa Auditing feature

--- src/main/java/com/practice/easyschool/EasySchoolApplication.java
+++ src/main/java/com/practice/easyschool/EasySchoolApplication.java
@@ -3,11 +3,13 @@ package com.practice.easyschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.practice.easyschool.repository")
 @EntityScan("com.practice.easyschool.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class EasySchoolApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/practice/easyschool/audit/AuditAwareImpl.java
@@ -0,0 +1,15 @@
+package com.practice.easyschool.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware {
+    @Override
+    public Optional getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+}

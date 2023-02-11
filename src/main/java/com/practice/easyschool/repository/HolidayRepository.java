b11a88c4466b6999746e613da771e2828f3aa9f9
Panks <pankajsingla012@gmail.com>
2023-02-11 07:41:43 +0530

    Removing un-used imports

--- src/main/java/com/practice/easyschool/repository/HolidayRepository.java
+++ src/main/java/com/practice/easyschool/repository/HolidayRepository.java
@@ -1,14 +1,9 @@
 package com.practice.easyschool.repository;
 
 import com.practice.easyschool.model.Holiday;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HolidayRepository extends CrudRepository<Holiday, String> {

9f9bd0c9eb24cb2b18f113fb20fdedb3591eefd5
Panks <pankajsingla012@gmail.com>
2023-03-14 11:53:54 +0530

    Adding Global Exception Rest Controller && Json Ignore for Audit Info

--- src/main/java/com/practice/easyschool/model/BaseEntity.java
+++ src/main/java/com/practice/easyschool/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.practice.easyschool.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -19,17 +20,21 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @JsonIgnore
     private LocalDateTime createdAt;
 
     @CreatedBy
     @Column(updatable = false)
+    @JsonIgnore
     private String createdBy;
 
     @LastModifiedDate
     @Column(insertable = false)
+    @JsonIgnore
     private LocalDateTime updatedAt;
 
     @LastModifiedBy
     @Column(insertable = false)
+    @JsonIgnore
     private String updatedBy;
 }
